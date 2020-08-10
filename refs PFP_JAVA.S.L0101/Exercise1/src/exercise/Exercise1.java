203644f2eec9f28a1584d7fcaa2957e159f809bc
BoCuoi98 <69208744+BoCuoi98@users.noreply.github.com>
2020-08-10 18:22:02 +0700

    Add files via upload

---
+++ refs PFP_JAVA.S.L0101/Exercise1/src/exercise/Exercise1.java
@@ -0,0 +1,11 @@
+package exercise;
+
+public class Exercise1 {
+
+	public static void main(String[] args) {
+		int number1 = 125;
+		int number2 = 2019;
+		System.out.println("Sum: " + (number1 + number2));
+	}
+
+}
